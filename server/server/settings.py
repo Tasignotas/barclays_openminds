ffbb3acc4095747add7b1cf7d0fcb3f09c3a1ddd
Marijonas <marpetr@gmail.com>
2013-11-21 21:08:24 +0000

    Login

--- server/server/settings.py
+++ server/server/settings.py
@@ -81,3 +81,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Authentication
+
+LOGIN_REDIRECT_URL = '/traders/profile/'
+LOGIN_URL = 'django.contrib.auth.views.login'
--- server/server/urls.py
+++ server/server/urls.py
@@ -8,5 +8,7 @@ urlpatterns = patterns('',
     url(r'^$', 'creative_exchange.views.trading', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^traders/login/$', 'django.contrib.auth.views.login'),
+
     url(r'^admin/', include(admin.site.urls)),
 )
