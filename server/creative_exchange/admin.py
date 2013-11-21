fbebc849f55fc927f0a9f1b0368192fba3ec3830
Marijonas <marpetr@gmail.com>
2013-11-21 16:41:25 +0000

    Offer model

--- server/creative_exchange/admin.py
+++ server/creative_exchange/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from creative_exchange import models
+
+admin.site.register(models.Offer)
--- server/creative_exchange/models.py
+++ server/creative_exchange/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Offer(models.Model):
+    ACTIONS = [('buy', 'Buy'), ('sell', 'Sell')]
+    stock_label = models.CharField(max_length=50)
+    action = models.CharField(max_length=10, choices=ACTIONS)
+    price = models.IntegerField(help_text='In cents')
+    quantity = models.IntegerField()
+    user = models.CharField(max_length=200)
+    timestamp = models.DateTimeField(auto_now_add=True)
