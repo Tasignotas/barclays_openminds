4e2edde0885136d4649027b1138275549ece0e5e
Ignotas Sulzenko S1137931 <s1137931@sms.ed.ac.uk>
2013-11-21 23:38:16 +0000

    Ajax for order book works!

--- server/server/urls.py
+++ server/server/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'creative_exchange.views.trading', name='home'),
     url(r'^traders/login/$', 'django.contrib.auth.views.login'),
+    url(r'^order_book', 'creative_exchange.views.order_book'),
     url(r'^traders/logout/$', 'django.contrib.auth.views.logout_then_login', kwargs={ 'login_url': '/' }),
     url(r'^admin/', include(admin.site.urls)),
 )
