48e6d15a625b30463308a6e04ad15d21cf597878
Marijonas <marpetr@gmail.com>
2013-11-21 23:40:48 +0000

    Order type select

--- server/creative_exchange/forms.py
+++ server/creative_exchange/forms.py
@@ -3,6 +3,20 @@ from django import forms
 from creative_exchange import models
 
 class OfferForm(forms.ModelForm):
+    ORDER_TYPES = [
+        ('market', 'Market Order'),
+        ('limit', 'Limit Order'),
+        ('day', 'Day Order (24 hours)'),
+        ('ioc', 'Immediate or Cancel'),
+        ('fok', 'Fill or Kill'),
+    ]
+    
     class Meta:
         model = models.Offer
         exclude = ('user',)
+        widgets = {
+            'action': forms.RadioSelect
+        }
+    
+    order_type = forms.ChoiceField(choices=ORDER_TYPES)
+    action = forms.ChoiceField(choices=models.Offer.ACTIONS, widget=forms.RadioSelect)
