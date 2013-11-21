5214815c4358b6e8ad947208d3948be581e968c5
Ignotas Sulzenko S1137931 <s1137931@sms.ed.ac.uk>
2013-11-21 23:08:09 +0000

    Adding ajax

--- server/creative_exchange/views.py
+++ server/creative_exchange/views.py
@@ -1,3 +1,6 @@
+import json
+
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 from creative_exchange import forms
@@ -28,3 +31,12 @@ def trading(request):
         trades = []
     offers = Offer.objects.order_by('stock_label', 'action', 'timestamp')
     return render(request, 'trading.html', dict(trade_form=form, trades=trades, offers=offers))
+    
+def offer_json(request):
+    answer = '<tr><th>Stock Label</th><th>Action</th><th>Price</th><th>Quantity</th><th>User</th><th>Timestamp</th></tr>'
+    offers = Offer.objects.order_by('stock_label', 'action', 'timestamp')
+    if len(offers) == 0:
+        return HttpResponse(answer + '<tr><td colspan="6" align="center">No offers available</td></tr>')
+    for offer in offers:
+        answer += '<tr><td>' + offer.stock_label + '</td><td>' + offer.get_action_display() + '</td><td>' + str(offer.price) + '</td><td>' + str(offer.quantity) + '</td><td>' + offer.user + '</td><td>' + offer.timestamp + '</td></tr>'
+    return HttpResponse(answer)
--- server/server/urls.py
+++ server/server/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'creative_exchange.views.trading', name='home'),
+    url(r'offer_json', 'creative_exchange.views.offer_json', name='data'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
