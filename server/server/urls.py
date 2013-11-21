3a395acc14829e567b945374e82b8214a7e62b11
Marijonas <marpetr@gmail.com>
2013-11-21 22:13:09 +0000

    Base template + login

--- server/server/urls.py
+++ server/server/urls.py
@@ -6,9 +6,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'creative_exchange.views.trading', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
     url(r'^traders/login/$', 'django.contrib.auth.views.login'),
+    url(r'^traders/logout/$', 'django.contrib.auth.views.logout_then_login', kwargs={ 'login_url': '/' }),
 
     url(r'^admin/', include(admin.site.urls)),
 )
