fbc8d272f58fc4325cd431dad009717361849fd9
Ignotas Sulzenko S1137931 <s1137931@sms.ed.ac.uk>
2013-11-22 00:23:56 +0000

    Optimized ajax requests and solved merge conflicts

--- server/creative_exchange/forms.py
+++ server/creative_exchange/forms.py
@@ -4,9 +4,25 @@ from django.contrib.auth.models import User
 from creative_exchange import models
 
 class OfferForm(forms.ModelForm):
+    ORDER_TYPES = [
+        ('market', 'Market Order'),
+        ('limit', 'Limit Order'),
+        ('day', 'Day Order (24 hours)'),
+        ('ioc', 'Immediate or Cancel'),
+        ('fok', 'Fill or Kill'),
+    ]
+    
     class Meta:
         model = models.Offer
         exclude = ('user',)
+        widgets = {
+            'action': forms.RadioSelect,
+            'price': forms.TextInput,
+            'quantity': forms.TextInput,
+        }
+    
+    order_type = forms.ChoiceField(choices=ORDER_TYPES)
+    action = forms.ChoiceField(choices=models.Offer.ACTIONS, widget=forms.RadioSelect)
 
 class TraderSelectForm(forms.Form):
     trader = forms.ModelChoiceField(User.objects.filter(is_staff=False), required=False)
