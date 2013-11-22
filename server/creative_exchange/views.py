f549336e689dfcbfbefdefaf05ddd1521c0e5b0d
Lukas <l.dirzys@yahoo.com>
2013-11-22 02:38:48 +0000

    New graphs

--- server/creative_exchange/views.py
+++ server/creative_exchange/views.py
@@ -45,7 +45,7 @@ def trader_test(request):
     form = forms.TraderSelectForm(request.GET)
     profit_for_stock = {}
     if form.is_valid() and form.cleaned_data['trader'] is not None:
-        for sign, trades in [(1, Trade.objects.filter(buyer=form.cleaned_data['trader'])), (-1, Trade.objects.filter(seller=form.cleaned_data['trader']))]:
+        for sign, trades in [(-1, Trade.objects.filter(buyer=form.cleaned_data['trader'])), (1, Trade.objects.filter(seller=form.cleaned_data['trader']))]:
             for trade in trades:
                 profit_for_stock[trade.stock_label] = profit_for_stock.get(trade.stock_label, 0) + sign * trade.price * trade.quantity
     data = zip(*profit_for_stock.items())
