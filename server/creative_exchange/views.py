7202848e511d5a9b1332321032bb846ccbdad9e8
Marijonas <marpetr@gmail.com>
2013-11-22 00:38:44 +0000

    Bootstrap

--- server/creative_exchange/views.py
+++ server/creative_exchange/views.py
@@ -26,7 +26,7 @@ def trading(request):
         form = forms.OfferForm(initial=initial)
     offers = Offer.objects.order_by('stock_label', 'action', 'timestamp')
     trades = Trade.objects.order_by('-timestamp')
-    return render(request, 'trading.html', dict(trade_form=form, trades=trades, offers=offers))
+    return render(request, 'trading.html', dict(trade_form=form, trades=trades, offers=offers, active_page='home'))
     
 def order_book(request):
     offers = Offer.objects.order_by('stock_label', 'action', 'timestamp')
@@ -39,5 +39,7 @@ def trader_test(request):
         for sign, trades in [(1, Trade.objects.filter(buyer=form.cleaned_data['trader'])), (-1, Trade.objects.filter(seller=form.cleaned_data['trader']))]:
             for trade in trades:
                 profit_for_stock[trade.stock_label] = profit_for_stock.get(trade.stock_label, 0) + sign * trade.price * trade.quantity
-    stock_labels, stock_profits = zip(*profit_for_stock.items())
-    return render(request, 'trader_profile.html', { 'trader_profile_form': form, 'stock_labels': stock_labels, 'stock_profits': stock_profits })
+    stock_labels, stock_profits = (), () #zip(*profit_for_stock.items())
+    return render(request, 'trader_profile.html',
+                  { 'trader_profile_form': form, 'stock_labels': stock_labels, 'stock_profits': stock_profits,
+                    'active_page': 'portfolio' })
